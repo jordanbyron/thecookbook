1a5e031095be4e2a9733e77b03b8c0df5168f20f
Jordan Byron <jordan.byron@gmail.com>
2015-01-25 17:48:49 -0500

    Force SSL in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  force_ssl if: :ssl_configured?
+
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def ssl_configured?
+    !(Rails.env.development? || Rails.env.test?)
   end
 end
